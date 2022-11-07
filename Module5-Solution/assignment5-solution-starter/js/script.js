da96eb0637166c6ae96b463ddcd1dd236858e5c5
Geolide <georgeomamomo@gmail.com>
2022-11-07 23:54:00 +0100

    small addition to our page.

--- Module5-Solution/assignment5-solution-starter/js/script.js
+++ Module5-Solution/assignment5-solution-starter/js/script.js
@@ -13,15 +13,15 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
 
 var dc = {};
 
-var homeHtmlUrl = "snippets/home-snippet.html";
+var homeHtmlUrl = "assignment5-solution-starter/snippets/home-snippet.html";
 var allCategoriesUrl =
   "https://davids-restaurant.herokuapp.com/categories.json";
-var categoriesTitleHtml = "snippets/categories-title-snippet.html";
-var categoryHtml = "snippets/category-snippet.html";
+var categoriesTitleHtml = "assignment5-solution-starter/snippets/categories-title-snippet.html";
+var categoryHtml = "assignment5-solution-starter/snippets/category-snippet.html";
 var menuItemsUrl =
   "https://davids-restaurant.herokuapp.com/menu_items.json?category=";
-var menuItemsTitleHtml = "snippets/menu-items-title.html";
-var menuItemHtml = "snippets/menu-item.html";
+var menuItemsTitleHtml = "assignment5-solution-starter/snippets/menu-items-title.html";
+var menuItemHtml = "assignment5-solution-starter/snippets/menu-item.html";
 
 // Convenience function for inserting innerHTML for 'select'
 var insertHtml = function (selector, html) {
@@ -32,7 +32,7 @@ var insertHtml = function (selector, html) {
 // Show loading icon inside element identified by 'selector'.
 var showLoading = function (selector) {
   var html = "<div class='text-center'>";
-  html += "<img src='images/ajax-loader.gif'></div>";
+  html += "<img src='assignment5-solution-starter/images/ajax-loader.gif'></div>";
   insertHtml(selector, html);
 };
 
